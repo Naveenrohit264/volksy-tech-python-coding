e262e2cdfa4f76845671a5311d484995186f6c0e
Naveenrohit264 <naveenp@brightcomgroup.com>
2022-12-23 17:24:07 +0000

    H

--- python-hello_world/7-add.py
+++ python-hello_world/7-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-def add(a,b):
-    sum=a+b
+def add(a, b):
+    sum=a + b
     return sum
 print(add(100, -2))
+print(add(-100, -2))
+print(add(0, 0))
+print(add(98, "Holberton"))
+print(add(1, 2))
