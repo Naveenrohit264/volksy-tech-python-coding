e8d6ae8ba83886cf8f97dab60816ae9fbe974c2e
Naveenrohit264 <naveenp@brightcomgroup.com>
2022-12-25 18:38:43 +0000

    hi

--- python-hello_world/8-pow.py
+++ python-hello_world/8-pow.py
@@ -1,22 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result= a** b
-    return result
-print(pow(2, 2))
-def pow(a, b):
-    result= a** b
-    return result
-print(pow(98, 2))
-def pow(a, b):
-    result= a** b
-    return result
-print(pow(98, 0))
-def pow(a,b):
-    result=a**b
-    return result
-print(pow(100, -2))
-def pow(a,b):
-    result=a**b
-    return result
-print(pow(-4, 5))
-
+    return a ** b
