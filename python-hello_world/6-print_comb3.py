046cac67a6e402bee9bc1cb99a55dbd2cdd0b902
Naveenrohit264 <naveenp@brightcomgroup.com>
2022-12-25 14:03:24 +0000

    hi

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if i == 1 and j == 2:
+        if i== 1 and j== 2:
             print("12")
         elif i < j:
-            print('{:d}{:d}'.format(i,j),end = ", ")
+            print('{:d}{:d}'.format(i, j),end = ", ")
